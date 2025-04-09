d1502fc55ac5e72e5a08ea45386c3c0d40a23260
collfreezboost001 <collfreezboost.co@gmail.com>
2025-04-10 00:58:10 +0530

    Update info.py

--- info.py
+++ info.py
@@ -96,8 +96,8 @@ NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", False))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
 
 # Token Verification Info :
-VERIFY = bool(environ.get('VERIFY', False))
-VERIFY_SECOND_SHORTNER = bool(environ.get('VERIFY_SECOND_SHORTNER', true))
+VERIFY = bool(environ.get('VERIFY', true))
+VERIFY_SECOND_SHORTNER = bool(environ.get('VERIFY_SECOND_SHORTNER', False))
 VERIFY_SHORTLINK_URL = environ.get('VERIFY_SHORTLINK_URL', 'get2short.com')
 VERIFY_SHORTLINK_API = environ.get('VERIFY_SHORTLINK_API', '833072d05fad8654241273e95d8e817ece1f0fb7')
 # if verify second shortner is True then fill below url and api

3679a9e18d3297c3a36d4989b907977ac0db5a88
collfreezboost001 <collfreezboost.co@gmail.com>
2025-04-10 01:21:21 +0530

    Update info.py

--- info.py
+++ info.py
@@ -96,7 +96,7 @@ NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", False))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
 
 # Token Verification Info :
-VERIFY = bool(environ.get('VERIFY', true))
+VERIFY = bool(environ.get('VERIFY', True))
 VERIFY_SECOND_SHORTNER = bool(environ.get('VERIFY_SECOND_SHORTNER', False))
 VERIFY_SHORTLINK_URL = environ.get('VERIFY_SHORTLINK_URL', 'get2short.com')
 VERIFY_SHORTLINK_API = environ.get('VERIFY_SHORTLINK_API', '833072d05fad8654241273e95d8e817ece1f0fb7')
